f0e0070abf3d53da13adf7ecaf652cbfa5b899a7
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2024-01-28 16:34:34 +0100

    factorize table confs

--- workflow/envs/r_shell.nix
+++ workflow/envs/r_shell.nix
@@ -6,6 +6,7 @@ let
   rpacks = with rPackages; [
     tidyverse
     geomtextpath
+    kableExtra
   ];
 in
 mkShell {
--- workflow/scripts/table_conferences.R
+++ workflow/scripts/table_conferences.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(knitr)
+library(kableExtra)
 
 args = commandArgs(trailingOnly=TRUE)
 filename = args[1]
@@ -20,14 +21,16 @@ per_conf <- df_unique %>%
   select(-rank) %>%
   group_by(conference) %>%
   summarize(
-    "Papers" = n(),
-    "Found PDFs" = sum(pdf_available),
-    "Repo URL" = sum(repo_url),
-    "Dead URL" = sum(dead_url),
+    "Accepted" = n(),
+    "Found PDF" = sum(pdf_available),
     "Artifact Section" = sum(artefact_section),
-    "Badge Available"  = sum(badge_avaible),
-    "Badge Functional" = sum(badge_evaluated_functional),
-    "Badge Reproduced" = sum(badge_reproduced)
+
+    "Specified" = sum(repo_url),
+    "Dead" = sum(dead_url),
+
+    "Available"  = sum(badge_avaible),
+    "Functional" = sum(badge_evaluated_functional),
+    "Reproduced" = sum(badge_reproduced)
   )
 
 total_conf <- per_conf %>%
@@ -44,4 +47,13 @@ linesep <- append(rep("", nrow(per_conf) - 1), "\\midrule")
 per_conf %>%
   rbind(total_conf) %>%
   rename(Conference = conference) %>%
-  knitr::kable(format="latex", align="lcccccccc", caption="Papers considered by conferences.", booktabs = TRUE, linesep = linesep, label="table:paper_confs", table.envir = "table*")
+  knitr::kable(
+      format="latex",
+      align="lcccccccc",
+      caption="Papers considered by conferences.",
+      booktabs = TRUE,
+      linesep = linesep,
+      label="table:paper_confs",
+      table.envir = "table*"
+  ) %>%
+  add_header_above(c(" " = 1, "Papers" = 3, "Artifact URL" = 2, "Badges" = 3))
